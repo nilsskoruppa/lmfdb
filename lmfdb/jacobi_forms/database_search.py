a188a000b127fe35d08f42d54e923cb3596c5e16
nils <nils.skoruppa@gmail.com>
2016-04-05 00:19:13 +0300

    Wrapped the responses to database queries into a response class for having a clear, easily managable  interface.

--- lmfdb/jacobi_forms/database_search.py
+++ lmfdb/jacobi_forms/database_search.py
@@ -5,7 +5,7 @@
 from parser import LatticeIndex
 import pymongo
 from database import DataBase
-
+import caller
 
 
 def prepare_query( qargs):
@@ -42,26 +42,48 @@ def prepare_query( qargs):
     return query
 
 
-# class Response:
+class Response:
 
-#     def __init__( self, qargs, context = 'thetablocks', limit = 100):
+    def __init__( self, qargs, context = 'thetablocks', limit = 100):
         
-#         self._qargs = qargs
-#         self._hits = db.count( query, collection = collection)
-       
-    
-def find( qargs, collection = 'thetablocks', limit = 10):
-    
-    query = prepare_query( qargs)
-    db = DataBase()
+        self._qargs = qargs
+        self._context = context
+        self.limit = limit
+        self._query = prepare_query( qargs)
+        self.hits = self._count()
+        self.data = self._find()
 
-    hits = db.count( query, collection = collection)
-    print '>>>>>>>>>>>>>> %d <<<<<<<<<<<<<<<<<' % hits
-    if hits > limit:
-        results = db.find( query, collection = collection, limit = limit)
-    else:
-        results = db.find( query, collection = collection)
-    if 'eigenforms' == collection:
-        from eigenform import Eigenform 
-        return [ Eigenform(**x) for x in results] + [query]
-    return [ x for x in results] + [query]
+    def _count( self):
+        db = DataBase()
+        return db.count( self._query, collection = self._context)
+
+    def _find( self):
+        db = DataBase()
+        if self.hits > self.limit:
+            results = db.find( self._query, collection = self._context, limit = self.limit)
+        else:
+            results = db.find( self._query, collection = self._context)
+        if 'eigenforms' == self._context:
+            from eigenform import Eigenform 
+            return [ Eigenform(**x) for x in results]
+        return [ x for x in results]
+
+
+                               
+                               
+# def find( qargs, collection = 'thetablocks', limit = 10):
+    
+#     query = prepare_query( qargs)
+#     db = DataBase()
+
+#     hits = db.count( query, collection = collection)
+#     print '>>>>>>>>>>>>>> %d <<<<<<<<<<<<<<<<<' % hits
+#     if hits > limit:
+#         results = db.find( query, collection = collection, limit = limit)
+#     else:
+#         results = db.find( query, collection = collection)
+#     if 'eigenforms' == collection:
+#         from eigenform import Eigenform 
+#         return [ Eigenform(**x) for x in results] + [query]
+#     return [ x for x in results] + [query]
+
--- lmfdb/jacobi_forms/jacobi_form.py
+++ lmfdb/jacobi_forms/jacobi_form.py
@@ -3,13 +3,10 @@
 # Author: Nils Skoruppa <nils.skoruppa@gmail.com>
 
 from flask import render_template, url_for, request, send_file
-# import input_parser
-# import dimensions
 import pickle
 import urllib
 from sage.all_cmdline import *
 import os
-# import sample
 from lmfdb.base import app
 from lmfdb.jacobi_forms import jf_page
 from lmfdb.jacobi_forms import jf_logger
@@ -18,30 +15,6 @@ import StringIO
 import caller
 import database_search
 
-# COLNS = None
-# DB = None
-
-
-# def rescan_collection():
-#     """
-#     Create and cache the instances of Collection.
-#     """
-#     from collection import Collection
-#     colns = dict()
-#     root = os.path.dirname( os.path.abspath(__file__))
-#     static = os.path.join( root, 'static')
-#     for a,b,files in os.walk( static):
-#         for f in files:
-#             try:
-#                 if f.endswith('.json'):
-#                     c = f[:-5]
-#                     # print c
-#                 colns[c] = Collection( c, location = static)
-#             except Exception as e:
-#                 print str(e)
-#                 pass
-#     global COLNS
-#     COLNS = colns
 
 
 @app.route('/JacobiForm/Q')
@@ -49,10 +22,6 @@ import database_search
 @app.route('/JacobiForm/Q/<page>')
 @app.route('/JacobiForm/Q/<page>/')
 def JacobiForm_Q_top_level( page = None):
-
-    # if request.args.get('empty_cache') or not COLNS:
-    #     # we trigger a (re)scan for available collections
-    #     rescan_collection()
 
     bread = [('Jacobi forms', url_for('JacobiForm_Q_top_level'))]        
 
@@ -70,8 +39,6 @@ def JacobiForm_Q_top_level( page = None):
         
         else:
             return prepare_main_page( bread)
-    # if page in COLNS:
-    #     return prepare_collection_page( COLNS[page], request.args, bread)
     if 'dimensions' == page:
         return prepare_dimensions_page( request.args, bread)
     if 'modules' == page:
@@ -82,17 +49,6 @@ def JacobiForm_Q_top_level( page = None):
         return prepare_singular_forms_page( request.args, bread)
     if 'eigenforms' == page:
         return prepare_eigenforms_page( request.args, bread)
-    # if 'search_results' == page:
-    #     return prepare_search_results_page( request.args, bread)
-    # # check whether there is a sample called page
-    # try:
-    #     a,b=page.split('.')
-    # except:
-    #     a=b=None
-    # sam = sample.Samples( { 'collection': a, 'name': b})
-    # if len( sam) > 0:
-    #     return prepare_sample_page( sam[0], request.args, bread)
-
     # return an error: better emit a 500    
     info = { 'error': 'Requested page does not exist' }
     return render_template("None.html", **info)
@@ -103,12 +59,7 @@ def JacobiForm_Q_top_level( page = None):
 ## HOME PAGE OF Jacobi FORMS
 ##########################################################
 def prepare_main_page( bread):
-    # info = { 'number_of_collections': len(COLNS),
-    #          'number_of_samples': len(sample.Samples( {})),
-    #          'cols': COLNS.values(),
-    #          'cols_with_forms': [COLNS[c] for c in COLNS if len(COLNS[c].members()) > 0],
-    #          'cols_comp_dims': [COLNS[c] for c in COLNS if COLNS[c].computes_dimensions()]
-    #          }
+
     info = {}
     info['learnmore'] = [
         ('Source of the data', url_for('JacobiForm_Q_top_level', page='jf-source')),
@@ -119,40 +70,6 @@ def prepare_main_page( bread):
                             title='Jacobi Forms',
                             bread=bread,
                             **info)
-
-
-
-##########################################################
-## SPECIFIC COLLECTION REQUEST
-##########################################################
-# def prepare_collection_page( col, args, bread):
-#     #info['learnmore'] += [ ('The spaces $'+col.latex_name()+'$', col.name()+'/basic')]
-
-#     mbs = col.members()
-#     info = { 'col': col,
-#              'forms': [ (k,
-#                          [(f.name(), f.field().degree())
-#                           for f in mbs if k == f.weight()])
-#                         for k in Set( f.weight() for f in mbs)],
-#              }
-#     dim_args = args.get( 'dim_args')
-#     if not dim_args:
-#         dim_args = col.dim_args_default
-#     if dim_args:
-#         try:
-#             dim_args = eval( dim_args)
-#             header, table = col.dimension( *dim_args)
-#             info.update({ 'col': col,
-#                           'dimensions': table,
-#                           'table_headers': header,
-#                           'dim_args': dim_args
-#                           })
-#         except Exception as e:
-#             info.update( {'error': str(e)})            
-#     bread.append( ('$'+col.latex_name()+'$', col.name()))
-#     return render_template("ModularForm_GSp4_Q_collection.html",
-#                            title='Siegel modular forms $'+col.latex_name()+'$',
-#                            bread=bread, **info)
 
 
 
@@ -215,9 +132,10 @@ def prepare_thetablocks_page( args, bread):
 
     try:
         query = json.loads( args)
-        results = database_search.find( query)
-        info.update( { 'table': results,
-                       'viewer': 'thetablocks/jf-table.html'
+        response = database_search.Response( query)
+        info.update( {
+            'response' : response,
+            'viewer': 'thetablocks/jf-table.html'
         })
     except Exception as e:
         info.update( {'error': '%s' % str(e)})
@@ -254,8 +172,8 @@ def prepare_eigenforms_page( args, bread):
 
     try:
         query = json.loads( args)
-        results = database_search.find( query, collection = 'eigenforms')
-        info.update( { 'table': results,
+        response = database_search.Response( query, context = 'eigenforms')
+        info.update( { 'response': response,
                        'viewer': 'eigenforms/jf-table.html'
         })
         info['friends'] =  [
@@ -267,80 +185,3 @@ def prepare_eigenforms_page( args, bread):
     return render_template( "eigenforms/jf-eigenforms.html",
                             title='Hecke eigenforms',
                             bread=bread, **info)
-
- 
-
-
-# ##########################################################
-# ## SEARCH RESULTS
-# ##########################################################
-# def prepare_search_results_page( args, bread):
-
-#     info = {'all':args}
-#     query = args.get('query')
-#     try:
-#         query = json.loads( query)
-#         results = sample.Samples( query)
-#         info.update( {'results': results})
-#     except Exception as e:
-#         info.update( {'error': '%s %s' % (str(e),query)})
-
-#     bread.append( ('search results', 'search_results'))
-#     return render_template( "ModularForm_GSp4_Q_search_results.html",
-#                             title='Siegel modular forms search results',
-#                             bread=bread, **info)
-
-
-
-# ##########################################################
-# ## SPECIFIC FORM REQUEST
-# ##########################################################
-# def prepare_sample_page( sam, args, bread):
-#     info = {'sam': sam, 'latex': latex}
-
-    
-#     info['evs_to_show'] = args.get( 'indices', [])
-#     if info['evs_to_show'] != []:
-#         try:
-#             info['evs_to_show'] = [ Integer(l) for l in info['evs_to_show'].split()]
-#         except Exception as e:
-#             info['error'] = 'list of l: %s' % str(e)
-#             info['evs_to_show'] = []
-
-#     info['fcs_to_show'] = args.get( 'dets', [])
-#     if info['fcs_to_show'] != []:
-#         try:
-#             info['fcs_to_show'] = [Integer(d) for d in info['fcs_to_show'].split()]
-#         except Exception as e:
-#             info['error'] = 'list of det(F): %s' % str(e)
-#             info['fcs_to_show'] = []
-
-#     null_ideal = sam.field().ring_of_integers().ideal(0)
-#     info['ideal_l'] = args.get( 'modulus', null_ideal)
-#     if info['ideal_l'] != 0:
-#         try:
-#             O = sam.field().ring_of_integers()
-#             id_gens = [O(str(b)) for b in info['ideal_l'].split()]
-#             info['ideal_l'] = O.ideal(id_gens)
-#         except Exception as e:
-#             info['error'] = 'list of generators: %s' % str(e)
-#             info['ideal_l'] = null_ideal
-
-#     # Hack to reduce polynomials and to handle non integral stuff
-#     if sam.representation() == '2':
-#         fun = info['ideal_l'].reduce
-#         def apple(x):
-#             try:
-#                 return fun(x)
-#             except:
-#                 try:
-#                     return x.parent()(dict( (i, fun( x[i])) for i in x.dict()))
-#                 except:
-#                     return 'Reduction undefined'
-#         info['ideal_l'].reduce = apple
-        
-#     bread.append( (sam.collection()[0] + '.' + sam.name(), '/' + sam.collection()[0] + '.' + sam.name()))
-#     return render_template( "ModularForm_GSp4_Q_sample.html",
-#                             title='Siegel modular forms sample ' + sam.collection()[0] + '.'+ sam.name(),
-#                             bread=bread, **info)
-
